2424fbee0f2e50cd29c7ad59e5bcf4836cd3f2ca
mr1337357 <miss@papaya>
2024-06-02 13:47:10 -0700

    tuned radio to longfast preset and started capturing data

--- NuggeTastic.ino
+++ NuggeTastic.ino
@@ -6,6 +6,11 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 
+#define RXD 38
+#define TXD 40
+#define RY_RESET 3
+#define LED 15
+
 #define CS_LORA 13
 #define MOSI 10
 #define MISO 8
@@ -42,11 +47,6 @@ void handle_lora(int size)
     data[i] = LoRa.read();
     Serial.printf("%02hhX ",data[i]);
   }
-  if(data[0] == 0x94 && data[1] == 0xc3)
-  {
-    display.println("probably meshtastic");
-    display.display();
-  }
   // print RSSI of packet
   Serial.print("' with RSSI ");
   Serial.println(LoRa.packetRssi());
@@ -54,25 +54,37 @@ void handle_lora(int size)
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
-
+  while(!Serial);
   display_start();
+  //ry_init();
 
   SPI.begin(SCK,MISO,MOSI,-1);
   LoRa.setPins(CS_LORA,RESET,IO0);
-  LoRa.setPreambleLength(16);
+
   Serial.println("LoRa Receiver");
   display.println("LoRa Receiver");
   display.display();
 
-  while(!LoRa.begin(915E6)) {
+  while(!LoRa.begin(906875000)) {
     Serial.println("Starting LoRa failed!");
     //while (1);
   }
+
+  LoRa.setSpreadingFactor(11);
+  LoRa.setSignalBandwidth(250E3);
+  LoRa.setCodingRate4(5);
+  LoRa.setPreambleLength(16);
+  LoRa.setSyncWord(0x2b);
+
+  LoRa.receive(0);
   Serial.println("LoRa started");
   display.println("LoRa started");
   display.display();
 }
+
+int32_t last;
+uint8_t outbound[256];
+int outindex = 0;
 
 void loop() {
   // try to parse packet
@@ -84,5 +96,28 @@ void loop() {
     display.setCursor(0, 0);
     display.printf("packet size %d\n",packetSize);
     display.display();
+  }
+  if(Serial.available())
+  {
+    char byte = Serial.read();
+    if(outindex < 256)
+    {
+      outbound[outindex++] = byte;
+      last = millis();
+    }
+  }
+  else
+  {
+    if(last + 1000 <= millis() && outindex > 0)
+    {
+      outbound[outindex] = 0;
+      display.printf("sending %s\n",outbound);
+      display.display();
+      LoRa.beginPacket(false);
+      LoRa.write(outbound,outindex);
+      LoRa.endPacket(false);
+      outindex = 0;
+      LoRa.receive(0);
+    }
   }
 }
